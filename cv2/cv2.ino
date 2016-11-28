27de52b1c0ad82f77950973b1cdc6510c7a8bba6
Jiri Horner <laeqten@gmail.com>
2016-11-28 19:07:57 +0100

    add comments to line follower

--- cv2/cv2.ino
+++ cv2/cv2.ino
@@ -1,5 +1,7 @@
 #include <Servo.h>
 
+/* simple line follower that uses 3-state logic. */
+
 #define button_pin 2
 #define infra_low 3
 #define infra_high 8
@@ -7,6 +9,7 @@
 Servo left;
 Servo right;
 int go = 0;
+// readings from infrared sensors
 bool infras[infra_high - infra_low];
 
 void setup()
@@ -19,7 +22,7 @@ void setup()
   pinMode(button_pin, INPUT_PULLUP);
   
   stopWheels();
-  // setup infaarray
+  // setup infa array
   for (int i = infra_low; i < infra_high; ++i) {
     pinMode(i, INPUT);
   }
@@ -29,26 +32,31 @@ bool buttonDown() {
   return !digitalRead(button_pin);
 }
 
+/* going forward, defaults to full speed */
 void goForward(int speed = 200) {
   left.write(1500 + speed);
   right.write(1500 - speed);
 }
 
+/* inplace turning */
 void turn(int slope) {
   left.write(1500 + slope);
   right.write(1500 + slope);
 }
 
+/* turning on smoother curve */
 void slopeTurn(int slope, int speed = 200) {
   left.write(1500 + speed + slope);
   right.write(1500 - speed + slope);
 }
 
+/* stops robot */
 void stopWheels() {
   left.write(1500);
   right.write(1500);
 }
 
+/* reading from infra red sensors */
 void debugInfra() {
   for (int i = 0; i < sizeof(infras); ++i) {
     Serial.print(infras[i]);
@@ -57,12 +65,14 @@ void debugInfra() {
   Serial.println();
 }
 
+/* reads infra red sensors to infras array */
 void readInfra() {
   for (int i = infra_low, j = 0; i < infra_high; ++i,++j) {
     infras[j] = digitalRead(i);
   }
 }
 
+/* indicates if any of the sensors is on the line */
 bool lineAhead() {
   bool res = 0;
   for (int i = infra_low; i < infra_high; ++i) {
@@ -71,14 +81,17 @@ bool lineAhead() {
   return res;
 }
 
+/* line left to the robt */
 bool lineLeft() {
   return !infras[1];
 }
 
+/* line right to the robt */
 bool lineRight() {
   return !infras[3];
 }
 
+/* line is properly aligned under robot */
 bool lineBeneath() {
   return !infras[2];
 }
@@ -97,6 +110,7 @@ void loop()
     return;
   }
 
+  // correct position according to line position
   if (lineBeneath()) {
     goForward(200);
   } else if (lineLeft()){
