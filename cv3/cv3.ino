3211ec92271cdce2b331614d2c813bb6c7ba9ed3
Jiri Horner <laeqten@gmail.com>
2016-12-05 19:26:39 +0100

    cv3
    line follower with markers
    
    * turns are too much agressive
    * turn after crossroad must be tested more

---
+++ cv3/cv3.ino
@@ -0,0 +1,161 @@
+#include <Servo.h>
+
+/* simple line follower that uses 3-state logic. */
+
+#define button_pin 2
+#define infra_low 3
+#define infra_high 8
+
+Servo left;
+Servo right;
+bool go_same_side = true;
+int mark = 0;
+// readings from infrared sensors
+bool infras[infra_high - infra_low];
+
+bool buttonDown() {
+  return !digitalRead(button_pin);
+}
+
+/* going forward, defaults to full speed */
+void goForward(int speed = 200) {
+  left.write(1500 + speed);
+  right.write(1500 - speed);
+}
+
+/* inplace turning */
+void turn(int slope) {
+  left.write(1500 + slope);
+  right.write(1500 + slope);
+}
+
+/* turning on smoother curve */
+void slopeTurn(int slope, int speed = 200) {
+  left.write(1500 + speed + slope);
+  right.write(1500 - speed + slope);
+}
+
+/* stops robot */
+void stopWheels() {
+  left.write(1500);
+  right.write(1500);
+}
+
+/* reading from infra red sensors */
+void debugInfra() {
+  for (int i = 0; i < sizeof(infras); ++i) {
+    Serial.print(infras[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  delay(500);
+}
+
+/* reads infra red sensors to infras array */
+void readInfra() {
+  for (int i = infra_low, j = 0; i < infra_high; ++i,++j) {
+    infras[j] = digitalRead(i);
+  }
+}
+
+/* indicates if any of the sensors is on the line */
+bool lineAhead() {
+  bool res = 0;
+  for (int i = infra_low; i < infra_high; ++i) {
+    res |= !infras[i];
+  }
+  return res;
+}
+
+/* line left to the robt */
+bool lineLeft() {
+  return !infras[1];
+}
+
+/* line right to the robt */
+bool lineRight() {
+  return !infras[3];
+}
+
+/* line is properly aligned under robot */
+bool lineBeneath() {
+  return !infras[2];
+}
+
+bool markRight() {
+  return !infras[4];
+}
+
+bool markLeft() {
+  return !infras[0];
+}
+
+bool stopLine() {
+  return markLeft() && markRight();
+}
+
+bool crossRoad() {
+  return (lineBeneath() && lineRight()) || (lineBeneath() && lineLeft());
+}
+
+void setup()
+{
+  // initialize serial and wait for port to open:
+  Serial.begin(115200);
+  left.attach(12);
+  right.attach(13);
+  // button
+  pinMode(button_pin, INPUT_PULLUP);
+  
+  stopWheels();
+  // setup infa array
+  for (int i = infra_low; i < infra_high; ++i) {
+    pinMode(i, INPUT);
+  }
+}
+
+const static int mark_turn_ratio = 80;
+const static int mark_delay = 30;
+const static int mark_speed = 40;
+
+const static int basic_turn_ratio = 100;
+const static int forward_speed = 60;
+
+void loop()
+{
+  readInfra();
+  // Serial.println(buttonDown());
+  // debugInfra();
+
+  if (buttonDown()) {
+    stopWheels();
+    delay(8000);
+    // go_same_side = false;
+  }
+
+  if (stopLine()) {
+    stopWheels();
+  }
+
+  // correct position according to line position
+  if (lineBeneath()) {
+    goForward(forward_speed);
+  } else if (lineLeft()){
+    slopeTurn(-1 * basic_turn_ratio, forward_speed);
+  } else if (lineRight()) {
+    slopeTurn(basic_turn_ratio, forward_speed);
+  } else { // finding line
+    slopeTurn(-30);
+  }
+  if (markLeft()) {
+    mark = -1;
+  } else if (markRight()) {
+    mark = 1;
+  }
+  if (crossRoad() && mark) {
+    slopeTurn(mark * mark_turn_ratio, mark_speed);
+    delay(mark_delay);
+    mark = 0;
+  }
+  delay(1);
+}
