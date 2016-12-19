bb304a9714065343de5f359cf47038a7a1085950
Jiri Horner <laeqten@gmail.com>
2016-12-19 19:45:07 +0100

    changes before christmas

--- cv3/cv3.ino
+++ cv3/cv3.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-/* simple line follower that uses 3-state logic. */
+/* line follower that follows hints */
 
 #define button_pin 2
 #define infra_low 3
@@ -9,13 +9,15 @@
 Servo left;
 Servo right;
 bool go_same_side = true;
-int mark = 0;
+signed char mark = 0;
+signed char crossroad_count = 0; // how many crossroads we have met yet (0 or 1). this
+                         // will be reseted after we are out of junction
 // readings from infrared sensors
 bool infras[infra_high - infra_low];
 
-bool buttonDown() {
-  return !digitalRead(button_pin);
-}
+typedef unsigned int uint;
+
+bool buttonDown() { return !digitalRead(button_pin); }
 
 /* going forward, defaults to full speed */
 void goForward(int speed = 200) {
@@ -53,7 +55,7 @@ void debugInfra() {
 
 /* reads infra red sensors to infras array */
 void readInfra() {
-  for (int i = infra_low, j = 0; i < infra_high; ++i,++j) {
+  for (int i = infra_low, j = 0; i < infra_high; ++i, ++j) {
     infras[j] = digitalRead(i);
   }
 }
@@ -68,45 +70,44 @@ bool lineAhead() {
 }
 
 /* line left to the robt */
-bool lineLeft() {
-  return !infras[1];
-}
+bool lineLeft() { return !infras[1]; }
 
 /* line right to the robt */
-bool lineRight() {
-  return !infras[3];
-}
+bool lineRight() { return !infras[3]; }
 
 /* line is properly aligned under robot */
-bool lineBeneath() {
-  return !infras[2];
+bool lineBeneath() { return !infras[2]; }
+
+bool markRight() { return !infras[4]; }
+
+bool markLeft() { return !infras[0]; }
+
+void maskLeft() { infras[1] = 1; }
+
+void maskRight() { infras[3] = 1; }
+
+void maskDirection(int direction) {
+  if (mark == -1) {
+    maskRight();
+  } else if (mark == 1) {
+    maskLeft();
+  }
 }
 
-bool markRight() {
-  return !infras[4];
-}
-
-bool markLeft() {
-  return !infras[0];
-}
-
-bool stopLine() {
-  return markLeft() && markRight();
-}
+bool stopLine() { return markLeft() && markRight(); }
 
 bool crossRoad() {
   return (lineBeneath() && lineRight()) || (lineBeneath() && lineLeft());
 }
 
-void setup()
-{
+void setup() {
   // initialize serial and wait for port to open:
   Serial.begin(115200);
   left.attach(12);
   right.attach(13);
   // button
   pinMode(button_pin, INPUT_PULLUP);
-  
+
   stopWheels();
   // setup infa array
   for (int i = infra_low; i < infra_high; ++i) {
@@ -114,15 +115,28 @@ void setup()
   }
 }
 
-const static int mark_turn_ratio = 80;
-const static int mark_delay = 30;
+const static int mark_turn_ratio = 40;
+const static int mark_delay = 100;
 const static int mark_speed = 40;
 
-const static int basic_turn_ratio = 100;
-const static int forward_speed = 60;
+const static int basic_turn_ratio = 70;
+const static int forward_speed = 40;
+const static uint crossroad_delay = 25000;
 
-void loop()
-{
+void lineFollow() {
+  // correct position according to line position
+  if (lineBeneath()) {
+    goForward(forward_speed);
+  } else if (lineLeft()) {
+    slopeTurn(-1 * basic_turn_ratio, forward_speed);
+  } else if (lineRight()) {
+    slopeTurn(basic_turn_ratio, forward_speed);
+  } else { // finding line
+    slopeTurn(-30);
+  }
+}
+
+void loop() {
   readInfra();
   // Serial.println(buttonDown());
   // debugInfra();
@@ -135,27 +149,45 @@ void loop()
 
   if (stopLine()) {
     stopWheels();
+    // return;
   }
 
-  // correct position according to line position
-  if (lineBeneath()) {
-    goForward(forward_speed);
-  } else if (lineLeft()){
-    slopeTurn(-1 * basic_turn_ratio, forward_speed);
-  } else if (lineRight()) {
-    slopeTurn(basic_turn_ratio, forward_speed);
-  } else { // finding line
-    slopeTurn(-30);
-  }
-  if (markLeft()) {
+  // marker
+  if (markLeft() && !crossroad_count) {
     mark = -1;
-  } else if (markRight()) {
+  } else if (markRight() && !crossroad_count) {
     mark = 1;
   }
-  if (crossRoad() && mark) {
+
+  // first cross road
+  if ((crossRoad() && mark) && !crossroad_count) {
+    crossroad_count = 1;
+
     slopeTurn(mark * mark_turn_ratio, mark_speed);
     delay(mark_delay);
-    mark = 0;
+    for(uint i = 0; i < crossroad_delay; ++i) {
+      readInfra();
+      maskDirection(mark);
+      lineFollow();
+    }
+    readInfra();
+    stopWheels();
+    delay(5000);
   }
-  delay(1);
+
+  // join junction
+  if (((markLeft() || markRight()) && mark) && crossroad_count) {
+    crossroad_count = 0;
+    for(uint i = 0; i < crossroad_delay/2; ++i) {
+      readInfra();
+      maskDirection(-1 * mark);
+      lineFollow();
+    }
+    mark = 0;
+    readInfra();
+    stopWheels();
+    delay(5000);
+  }
+
+  lineFollow();
 }
