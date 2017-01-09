c6e1d4c10b44dc053156858c49574dbdf011c9cf
Jiri Horner <laeqten@gmail.com>
2017-01-09 18:36:10 +0100

    reimplement junction code

--- cv3/cv3.ino
+++ cv3/cv3.ino
@@ -8,10 +8,8 @@
 
 Servo left;
 Servo right;
-bool go_same_side = true;
+signed char go_same_side = true;
 signed char mark = 0;
-signed char crossroad_count = 0; // how many crossroads we have met yet (0 or 1). this
-                         // will be reseted after we are out of junction
 // readings from infrared sensors
 bool infras[infra_high - infra_low];
 
@@ -78,14 +76,38 @@ bool lineRight() { return !infras[3]; }
 /* line is properly aligned under robot */
 bool lineBeneath() { return !infras[2]; }
 
+/* we have met indication mark on the right */
 bool markRight() { return !infras[4]; }
 
+/* we have met indication mark on the left */
 bool markLeft() { return !infras[0]; }
 
+/* clears reading from sensors left to the line, just like there is an empty
+ * space */
 void maskLeft() { infras[1] = 1; }
 
+/* clears reading from sensors right to the line, just like there is an empty
+ * space */
 void maskRight() { infras[3] = 1; }
 
+/* clears reading from sensors reading right marks, just like there is no mark
+ */
+void maskMarkRight() { infras[4] = 1; }
+
+/* clears reading from sensors reading left marks, just like there is no mark */
+void maskMarkLeft() { infras[0] = 1; }
+
+/* clears reading from both sensors reading both marks, just like there are no
+ * marks */
+void maskMarks() {
+  maskMarkLeft();
+  maskMarkRight();
+}
+
+/* true if there is mark on the left or right */
+bool anyMark() { return markLeft() || markRight(); }
+
+/* works like maskRight or maskLeft, according to direction */
 void maskDirection(int direction) {
   if (mark == -1) {
     maskRight();
@@ -94,8 +116,10 @@ void maskDirection(int direction) {
   }
 }
 
+/* returns true if robot is on stopline */
 bool stopLine() { return markLeft() && markRight(); }
 
+/* returns true if robot detected crossroad */
 bool crossRoad() {
   return (lineBeneath() && lineRight()) || (lineBeneath() && lineLeft());
 }
@@ -153,40 +177,41 @@ void loop() {
   }
 
   // marker
-  if (markLeft() && !crossroad_count) {
+  if (markLeft()) {
     mark = -1;
-  } else if (markRight() && !crossroad_count) {
+  } else if (markRight()) {
     mark = 1;
   }
 
-  // first cross road
-  if ((crossRoad() && mark) && !crossroad_count) {
-    crossroad_count = 1;
-
+  // junction
+  if (crossRoad() && mark) {
     slopeTurn(mark * mark_turn_ratio, mark_speed);
     delay(mark_delay);
-    for(uint i = 0; i < crossroad_delay; ++i) {
+    // first junction
+    for (uint i = 0; i < crossroad_delay; ++i) {
       readInfra();
       maskDirection(mark);
       lineFollow();
     }
-    readInfra();
     stopWheels();
-    delay(5000);
-  }
-
-  // join junction
-  if (((markLeft() || markRight()) && mark) && crossroad_count) {
-    crossroad_count = 0;
-    for(uint i = 0; i < crossroad_delay/2; ++i) {
+    delay(2000);
+    // line between
+    bool expect_crossroad = false;
+    while (!crossRoad() && !expect_crossroad) {
       readInfra();
-      maskDirection(-1 * mark);
+      expect_crossroad |= anyMark();
+      maskMarks();
+      lineFollow();
+    }
+    stopWheels();
+    delay(2000);
+    // joining junction
+    for (uint i = 0; i < crossroad_delay; ++i) {
+      readInfra();
+      maskDirection(mark);
       lineFollow();
     }
     mark = 0;
-    readInfra();
-    stopWheels();
-    delay(5000);
   }
 
   lineFollow();
