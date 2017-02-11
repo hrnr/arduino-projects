7f7773a0910a9bb375331c50981b6ac40632f219
Jiri Horner <laeqten@gmail.com>
2017-02-11 15:41:47 +0100

    hardened input parsing
    * detect invalid input
    * add more input files
    * accept lowecase formatted input

--- grid/grid.ino
+++ grid/grid.ino
@@ -7,6 +7,8 @@
 #define infra_low 3
 #define infra_high 8
 
+#define LENGTH(x) (sizeof x / sizeof x[0])
+
 struct Command {
   // where to go first
   bool row_first;
@@ -28,6 +30,8 @@ struct Command {
   }
 
   bool empty() { return time == 0; }
+
+  static Command createEmpty() { return {0, 0, 0, 0}; }
 };
 
 struct Status {
@@ -37,6 +41,10 @@ struct Status {
   unsigned char x;
   unsigned char y;
   Orientation orientation;
+
+  bool empty() { return x == 255 && y == 255 && orientation == 5; }
+
+  static Status createEmpty() { return {255, 255, Orientation(5)}; }
 };
 
 /* globals */
@@ -71,14 +79,22 @@ void waitButtonPress() {
   delay(1000);
 }
 
-/* read 1 command from serial line */
+/* read 1 command from serial line. on error returns empty Command */
 Command parseCommand() {
   // read first 2 characters - coordinates
   char a1 = Serial.read();
   char a2 = Serial.read();
   // read time
   unsigned int time = Serial.parseInt();
-  bool row_first = !isalpha(a1);
+  // handle invalid commands
+  if (time <= 0 || !(isAlpha(a1) ^ isAlpha(a2)) ||
+      !(isDigit(a1) ^ isDigit(a2))) {
+    return Command::createEmpty();
+  }
+  bool row_first = !isAlpha(a1);
+  // normalize letters to upper case
+  a1 = toupper(a1);
+  a2 = toupper(a2);
 
   // create structure
   unsigned char x, y;
@@ -94,11 +110,27 @@ Command parseCommand() {
   return {row_first, x, y, time};
 }
 
+/* reads serial input until there is a character available other than whitespace
+ */
+void skipWhiteSpace() {
+  // skip whitespace
+  while (Serial.available() && isspace(Serial.peek())) {
+    Serial.read();
+  }
+}
+
 /* read special status line from serial line */
 Status parseStatus() {
   char a1 = Serial.read();
   char a2 = Serial.read();
   char a3 = Serial.read();
+
+  if (!isAlpha(a1) || !isDigit(a2) || !isAlpha(a3)) {
+    return Status::createEmpty();
+  }
+  // normalize to uppercase
+  a1 = toupper(a1);
+  a3 = toupper(a3);
 
   Status status;
   status.x = a1 - 'A';
@@ -116,28 +148,41 @@ Status parseStatus() {
   case 'W':
     status.orientation = Status::WEST;
     break;
+  default:
+    return Status::createEmpty();
   }
 
   return status;
 }
 
-void readSchedule() {
+/* parses schedule send via serial. returns false if shedule is invalid. */
+bool readSchedule() {
+  skipWhiteSpace();
   // first line is special
   status = parseStatus();
+  if (status.empty()) {
+    return false;
+  }
   // zero out current schedule
   memset(schedule, 0, sizeof(schedule));
-  int i = 0;
+  unsigned char i = 0;
   while (Serial.available()) {
-    // skip whitespace
-    while (isspace(Serial.peek())) {
-      Serial.read();
+    if (i >= LENGTH(schedule)) {
+      Serial.println("maximum length of schedule exceeded. some commands might "
+                     "be stripped.");
+      return true;
     }
-
+    skipWhiteSpace();
     // read command
     if (Serial.available()) {
       schedule[i++] = parseCommand();
+      if (schedule[i - 1].empty()) {
+        return false;
+      }
     }
   }
+
+  return true;
 }
 
 void saveSchedule() {
@@ -282,9 +327,14 @@ void setup() {
   // wait 15 seconds to give user a chance to send new commands
   delay(15 * 1000);
   if (Serial.available()) {
-    Serial.println("commands received, writing new commands.");
-    readSchedule();
-    saveSchedule();
+
+    if (readSchedule()) {
+      Serial.println("commands received, writing new commands.");
+      saveSchedule();
+    } else {
+      Serial.println("invalid schedule. loading EEPROM contents.");
+      loadSchedule();
+    }
   } else {
     Serial.println("no commands, loading EEPROM contents.");
     loadSchedule();
